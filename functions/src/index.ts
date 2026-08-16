07c54cb203cd0ab647b34c821ca9d8e720a434c7
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 18:34:28 +0200

    feat: export guarded account deletion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ export { advanceSwapTransaction } from './callables/advance-swap-transaction.js'
 export { analyzeWardrobeItem } from './callables/analyze-wardrobe-item.js';
 export { cancelSwapOffer } from './callables/cancel-swap-offer.js';
 export { createSwapListing } from './callables/create-swap-listing.js';
+export { deleteMyAccount } from './callables/delete-my-account.js';
 export { exportMyData } from './callables/export-my-data.js';
 export { getAccountDeletionReadiness } from './callables/get-account-deletion-readiness.js';
 export { getOutfitWeather } from './callables/get-outfit-weather.js';
