a2fa9498fb461a1d4adbb62f486462889e09544c
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 14:45:00 +0200

    feat: export OmniSwap lifecycle commands

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 export { analyzeWardrobeItem } from './callables/analyze-wardrobe-item.js';
+export { cancelSwapOffer } from './callables/cancel-swap-offer.js';
 export { createSwapListing } from './callables/create-swap-listing.js';
 export { getOutfitWeather } from './callables/get-outfit-weather.js';
 export { respondSwapOffer } from './callables/respond-swap-offer.js';
 export { sendSwapOffer } from './callables/send-swap-offer.js';
+export { setSwapListingStatus } from './callables/set-swap-listing-status.js';
