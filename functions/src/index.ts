e1c5d29e42df24652141476c6e8a9c840fc73e9b
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 18:20:37 +0200

    feat: export push delivery backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,13 +5,17 @@ export { createSwapListing } from './callables/create-swap-listing.js';
 export { getOutfitWeather } from './callables/get-outfit-weather.js';
 export { markNotificationRead } from './callables/mark-notification-read.js';
 export { openSwapDispute } from './callables/open-swap-dispute.js';
+export { registerPushDevice } from './callables/register-push-device.js';
 export { respondSwapOffer } from './callables/respond-swap-offer.js';
 export { sendSwapOffer } from './callables/send-swap-offer.js';
 export { setSwapListingStatus } from './callables/set-swap-listing-status.js';
 export { setUserBlock } from './callables/set-user-block.js';
 export { submitReport } from './callables/submit-report.js';
 export { submitSwapReview } from './callables/submit-swap-review.js';
+export { unregisterPushDevice } from './callables/unregister-push-device.js';
 
+export { onNotificationCreatedPushDelivery } from './notifications/push-delivery.js';
+export { processExpoPushReceipts } from './notifications/push-receipts.js';
 export {
   onSwapDisputeCreated,
   onSwapOfferCreated,
