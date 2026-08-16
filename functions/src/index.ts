2076c8e91a57d9de33add62d0abcf910381cf2ef
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 14:52:00 +0200

    feat: export two-party OmniSwap progress command

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+export { advanceSwapTransaction } from './callables/advance-swap-transaction.js';
 export { analyzeWardrobeItem } from './callables/analyze-wardrobe-item.js';
 export { cancelSwapOffer } from './callables/cancel-swap-offer.js';
 export { createSwapListing } from './callables/create-swap-listing.js';
