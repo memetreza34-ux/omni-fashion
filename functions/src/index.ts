0e0d457f50a5f39af50eb9cb0af0225a158446ea
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 18:31:43 +0200

    feat: export privacy lifecycle foundation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@ export { advanceSwapTransaction } from './callables/advance-swap-transaction.js'
 export { analyzeWardrobeItem } from './callables/analyze-wardrobe-item.js';
 export { cancelSwapOffer } from './callables/cancel-swap-offer.js';
 export { createSwapListing } from './callables/create-swap-listing.js';
+export { exportMyData } from './callables/export-my-data.js';
+export { getAccountDeletionReadiness } from './callables/get-account-deletion-readiness.js';
 export { getOutfitWeather } from './callables/get-outfit-weather.js';
 export { listModerationQueue } from './callables/list-moderation-queue.js';
 export { listRecoveryQueue } from './callables/list-recovery-queue.js';
