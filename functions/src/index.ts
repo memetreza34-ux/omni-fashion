369252ed018bb44079cbcd1e11f28c98f8f7bd9f
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 14:34:57 +0200

    feat: export trusted OmniSwap offer commands

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
 export { analyzeWardrobeItem } from './callables/analyze-wardrobe-item.js';
 export { createSwapListing } from './callables/create-swap-listing.js';
 export { getOutfitWeather } from './callables/get-outfit-weather.js';
+export { respondSwapOffer } from './callables/respond-swap-offer.js';
+export { sendSwapOffer } from './callables/send-swap-offer.js';
