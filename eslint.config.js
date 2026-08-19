cab5716cd12caef5a6ad8aa9c11cde03118791d9
memetreza34-ux <memetreza34@gmail.com>
2026-08-19 07:34:50 +0200

    quality: add Expo SDK 57 ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  globalIgnores([
+    '.expo/*',
+    'dist/*',
+    'dist-ci/*',
+    'functions/lib/*',
+    'coverage/*',
+  ]),
+  expoConfig,
+]);
